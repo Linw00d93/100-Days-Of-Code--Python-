e15136e979da3d4d27a973f4ab9cd6598c8eff8b
Linwood <Linwood93@gmail.com>
2022-01-10 18:57:46 -0500

    Update lol

--- Day001.py
+++ Day001.py
@@ -7,8 +7,16 @@
  string = "my name is"
  character = 'a'
  print(number)
+
 """
 print("Welcome to the Band Generator!")
 city = input("What is the name of the city you are from: \n")
-pet_name = input("What is your pet's name or nickname: \n")
-print("Your band name is " + city + "" + pet_name) 
+petName = input("What is your pet's name or nickname: \n")
+print("Your band name is " + city + " " + petName) 
+
+#Letter counter 
+#In real life i would use a function to count the number of letters in a string and also check to seee if the input has numbers,spaces, or special characters.
+#it would take longer to excute but would have a correct answer 
+
+lettersInName = len(input("What is your name: \n"))
+print("Your name has " + str(lettersInName) + " letters in it.")
