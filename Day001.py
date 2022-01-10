244b27b8b17885aa18e1cf575c4bd0b1d0c94db6
Linwood <Linwood93@gmail.com>
2022-01-10 18:07:21 -0500

    Day 001

---
+++ Day001.py
@@ -0,0 +1,14 @@
+#Day 001 
+#Band Generator
+"""
+ print("Hello World")
+ number = 1 
+ boolean = true
+ string = "my name is"
+ character = 'a'
+ print(number)
+"""
+print("Welcome to the Band Generator!")
+city = input("What is the name of the city you are from: \n")
+pet_name = input("What is your pet's name or nickname: \n")
+print("Your band name is " + city + "" + pet_name) 
